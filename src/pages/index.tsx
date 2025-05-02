44792a43077c69ac93234a00ef03f149690fe616
KingJulien404 <julien.hafner@protonmail.com>
2025-05-01 21:32:44 -0400

    Fix: added default page component to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,9 @@
-// Entry point with ScoutFlowApp
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white text-3xl">
+      👋 Welcome to ScoutFlow
+    </main>
+  );
+}
