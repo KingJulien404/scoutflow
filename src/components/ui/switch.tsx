adf0aaef4e3336f7905ca5980d9538b3ffc3f803
KingJulien404 <julien.hafner@protonmail.com>
2025-05-02 14:27:31 -0400

    Add Switch component

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,1 +1,14 @@
-// ShadCN Switch Component
+import * as React from "react";
+
+export function Switch({ checked, onChange }: { checked: boolean; onChange: () => void }) {
+  return (
+    <label className="inline-flex items-center cursor-pointer">
+      <span className="mr-2 text-sm">Off</span>
+      <input type="checkbox" className="hidden" checked={checked} onChange={onChange} />
+      <div className="w-10 h-5 bg-gray-300 rounded-full shadow-inner">
+        <div className={`w-5 h-5 bg-white rounded-full transform transition ${checked ? "translate-x-5" : ""}`} />
+      </div>
+      <span className="ml-2 text-sm">On</span>
+    </label>
+  );
+}
