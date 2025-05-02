713830492480878934f02dbb0b1bb53b7831136a
KingJulien404 <julien.hafner@protonmail.com>
2025-05-02 14:22:50 -0400

    Force add UI component updates

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,1 +1,9 @@
-// ShadCN Button Component
+import * as React from "react";
+
+export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition" {...props}>
+      {children}
+    </button>
+  );
+}
