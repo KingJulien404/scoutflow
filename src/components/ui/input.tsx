8be222342f90467c4b02e8c8ac7b2ab55c2ad141
KingJulien404 <julien.hafner@protonmail.com>
2025-05-02 14:27:05 -0400

    Add Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,1 +1,10 @@
-// ShadCN Input Component
+import * as React from "react";
+
+export function Input({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      {...props}
+    />
+  );
+}
