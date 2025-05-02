620527bc81d452e47eb2b9ccbca7f6e6bc73b5be
KingJulien404 <julien.hafner@protonmail.com>
2025-05-01 23:09:10 -0400

    fix: added nav code content

---
+++ src/components/ui/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="w-full p-4 bg-black text-white flex justify-between items-center">
+      <h1 className="text-xl font-bold">ScoutFlow</h1>
+      <div className="space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/players">Players</Link>
+        <Link href="/compare">Compare</Link>
+        <Link href="/settings">Settings</Link>
+      </div>
+    </nav>
+  );
+}
